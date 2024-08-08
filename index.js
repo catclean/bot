726f6dc33c83aa372d85bc1e79a394ded6bf8fd8
catclean <catclean.medan@gmail.com>
2024-08-08 21:45:41 +0700

    everything has change

--- index.js
+++ index.js
@@ -2,44 +2,14 @@ const fs = require('node:fs');
 
 const file = JSON.parse(fs.readFileSync('contacts.json', 'utf-8'));
 const contacts = file.contacts;
-
-const messages = JSON.parse(fs.readFileSync('messages.json', 'utf-8'));
+const replyMessages = JSON.parse(fs.readFileSync('./db/reply-messages.json', 'utf-8'));
+const clientMessagesData = JSON.parse(fs.readFileSync('./db/client-message-data.json', 'utf-8'));
 
 const { Client, LocalAuth, MessageMedia} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const caption = '*[PROMO SPESIAL]*\n' +
-    'Layanan Grooming Home Service.\n' +      
-    '*Discount 20%* untuk *SEMUA* jenis grooming!\n' +
-    '\n' +
-    '*Harga Normal:*\n' +
-    '- Basic Groom: Rp 55.000\n' +
-    '- Medicated Groom: Rp 65.000\n' +        
-    '- Anti Flea Groom: Rp 75.000\n' +        
-    '- Complete Groom: Rp 90.000\n' +
-    '\n' +
-    '*Harga Promo:*\n' +
-    '- Basic Groom: Rp 45.000\n' +
-    '- Medicated Groom: Rp 50.000\n' +        
-    '- Anti Flea Groom: Rp 60.000\n' +        
-    '- Complete Groom: Rp 75.000\n' +
-    '\n' +
-    'Mengapa harus grooming di tempat kami?\n' +
-    '✅ Tidak perlu ke Petshop untuk grooming\n' +
-    '✅ Dipantau langsung dirumah Anda\n' +   
-    '✅ Staf berpengalaman dan terlatih\n' +  
-    '✅ Suasana nyaman untuk kucing Anda\n' + 
-    '✅ Hemat waktu & tenaga\n' +
-    '✅ Hasil grooming maksimal\n' +
-    '\n' +
-    'Jangan lewatkan kesempatan ini! Promo berlaku hingga _21 Agustus 2024_.\n' +
-    '_Syarat & ketentuan berlaku._.\n' +
-    '\n' +
-    'Tunggu apa lagi? Hubungi kami sekarang! \n' +
-    '�📞 WhatsApp: 087883977436\n' +
-    '�📸 Instagram: @catclean.medan';
 
-const orderText = 'Terimakasih sudah order jasa kami.\n'+
-    'Silahkan isi formulir dibawah'
+let chatId;
+let data = {}
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -51,55 +21,25 @@ client.on('ready', () => {
 
 client.on('message', async(message) => {
     const media = MessageMedia.fromFilePath('img/feed-1.png');
-    const body = message.body.toLocaleLowerCase();
-    const chat = client.getChatById(message.from);
+    const body = message.body;
+    const from = message.from;
+    const type = message.type;
+    const ts = message.timestamp * 1000;
+    const chat = await client.getChatById(message.from);
 
-    if(body == '!send'){
-        contacts.map((contact)=>{
-            let phoneNum;
-            const firstNum = contact.split('')[0]
-            if (contact.length > 9) {
-                if (firstNum !='6') {
-                    if (firstNum == '0') {
-                        const char = contact.substr(1);
-                        phoneNum = "62"+char+"@c.us"
-                        if (phoneNum.includes('628')) {
-                            phoneNum = phoneNum
-                            client.sendMessage(phoneNum.toString(), media, {caption})
-                            console.log('Ok!');
-                        }
-                    }else{
-                        phoneNum = "62"+contact+"@c.us"
-                        if (phoneNum.includes('628')) {
-                            phoneNum = phoneNum
-                            client.sendMessage(phoneNum.toString(), media, {caption})
-                            console.log('Ok!');
-                        }
-                    }
-                }else{
-                    if (contact.includes('628')) {
-                        phoneNum = contact+"@c.us"
-                        client.sendMessage(phoneNum.toString(), media, {caption})
-                        console.log('Ok!');
-                    }
-                }
-            }
-        })
+    if (type == 'chat') {
+        if (chatId == from) {
+            if (body == '1') {client.sendMessage(from, replyMessages.adm)}
+            if (body == '2') {client.sendMessage(from, replyMessages.list)}
+            if (body == '3') {client.sendMessage(from, replyMessages.promo)}
+            if (body == '4') {client.sendMessage(from, replyMessages.form)}
+            if (body == '5') {client.sendMessage(from, replyMessages.address)}
+            if (body == '6') {client.sendMessage(from, replyMessages.term)}
+        }else{
+            client.sendMessage(from, replyMessages.greeting);
+            chatId = from;
+        }
     }
-    if (body == '2') {
-        client.sendMessage(message.from, messages.list);
-    }
-    if (body == '3') {
-        client.sendMessage(message.from, messages.promo);
-    }
-    if (body == '4') {
-        client.sendMessage(message.from, messages.form);
-    }
-    if (body == '5') {
-        client.sendMessage(message.from, messages.address);
-    }
-    client.sendMessage(message.from, messages.greeting)
-    console.log(chat);
 })
 
 client.on('qr', qr => {
@@ -110,7 +50,39 @@ client.initialize();
 
 
 
+// const caption = '*[PROMO SPESIAL]*\n' +
+//     'Layanan Grooming Home Service.\n' +      
+//     '*Discount 20%* untuk *SEMUA* jenis grooming!\n' +
+//     '\n' +
+//     '*Harga Normal:*\n' +
+//     '- Basic Groom: Rp 55.000\n' +
+//     '- Medicated Groom: Rp 65.000\n' +        
+//     '- Anti Flea Groom: Rp 75.000\n' +        
+//     '- Complete Groom: Rp 90.000\n' +
+//     '\n' +
+//     '*Harga Promo:*\n' +
+//     '- Basic Groom: Rp 45.000\n' +
+//     '- Medicated Groom: Rp 50.000\n' +        
+//     '- Anti Flea Groom: Rp 60.000\n' +        
+//     '- Complete Groom: Rp 75.000\n' +
+//     '\n' +
+//     'Mengapa harus grooming di tempat kami?\n' +
+//     '✅ Tidak perlu ke Petshop untuk grooming\n' +
+//     '✅ Dipantau langsung dirumah Anda\n' +   
+//     '✅ Staf berpengalaman dan terlatih\n' +  
+//     '✅ Suasana nyaman untuk kucing Anda\n' + 
+//     '✅ Hemat waktu & tenaga\n' +
+//     '✅ Hasil grooming maksimal\n' +
+//     '\n' +
+//     'Jangan lewatkan kesempatan ini! Promo berlaku hingga _21 Agustus 2024_.\n' +
+//     '_Syarat & ketentuan berlaku._.\n' +
+//     '\n' +
+//     'Tunggu apa lagi? Hubungi kami sekarang! \n' +
+//     '📞 WhatsApp: 087883977436\n' +
+//     '📸 Instagram: @catclean.medan';
 
+// const orderText = 'Terimakasih sudah order jasa kami.\n'+
+//     'Silahkan isi formulir dibawah'
 
 
 
@@ -141,4 +113,37 @@ client.initialize();
         //         client.sendMessage('6285834323869@c.us', contact)
         //         console.log('message has been send by 62');
         //     }
+        // }
+
+        // if(body == '!send'){
+        //     contacts.map((contact)=>{
+        //         let phoneNum;
+        //         const firstNum = contact.split('')[0]
+        //         if (contact.length > 9) {
+        //             if (firstNum !='6') {
+        //                 if (firstNum == '0') {
+        //                     const char = contact.substr(1);
+        //                     phoneNum = "62"+char+"@c.us"
+        //                     if (phoneNum.includes('628')) {
+        //                         phoneNum = phoneNum
+        //                         client.sendMessage(phoneNum.toString(), media, {caption})
+        //                         console.log('Ok!');
+        //                     }
+        //                 }else{
+        //                     phoneNum = "62"+contact+"@c.us"
+        //                     if (phoneNum.includes('628')) {
+        //                         phoneNum = phoneNum
+        //                         client.sendMessage(phoneNum.toString(), media, {caption})
+        //                         console.log('Ok!');
+        //                     }
+        //                 }
+        //             }else{
+        //                 if (contact.includes('628')) {
+        //                     phoneNum = contact+"@c.us"
+        //                     client.sendMessage(phoneNum.toString(), media, {caption})
+        //                     console.log('Ok!');
+        //                 }
+        //             }
+        //         }
+        //     })
         // }
